a65287b68ef6daaeec19684d86fe148ac7e618c5
Amine Amhoume <amhoume@gmail.com>
2021-03-31 16:24:35 +0100

    now I can click on league name and see matches

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-var leagueTitle = document.getElementsByClassName('league-name')
-var leagueMatche = Array.from(document.getElementsByClassName('league-mt'))
+const leagueTitle = document.getElementById('league-name')
+const leagueMatche = Array.from(document.querySelectorAll('.league-mt'))
+const league = document.querySelector('.league')
 
-leagueTitle.addEventListener('click', () => {
-    leagueMatche.forEach(matche => matche.style.display = 'auto');
-})
+function addDisplay() {
+    leagueMatche.forEach(matche => {
+        if( matche.style.opacity === "0"){
+            matche.style.opacity = "1"
+            league.classList.add('show')
+        }else if( matche.style.opacity === "1"){
+            matche.style.opacity = "0"
+            league.classList.remove('show')
+        }else{
+            matche.style.opacity = "1"
+        }
+        
+            
+    });
+}
+
+leagueTitle.addEventListener('click', addDisplay)
