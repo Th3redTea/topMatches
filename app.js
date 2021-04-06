7ac080d1e96ed91b6ac4b7112dc658e3127a0162
Amine Amhoume <amhoume@gmail.com>
2021-04-06 10:08:38 +0100

    front page is 100M% responsive

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 
 const barMenu = document.getElementById('bar-menu')
-const navLinks = document.getElementById('nav-links')
+const navLinks = document.querySelector('.nav-links')
+const movileView = window.matchMedia("(max-width:640px)")
 
 barMenu.addEventListener('click', () => {
 
-    if ( navLinks.style.width === '0%' && navLinks.style.visibility === "hidden"){
         navLinks.style.width = '40%';
         navLinks.style.visibility = "visible";
-    }else if (navLinks.style.width === '40%' && navLinks.style.visibility === "visible") {
-        navLinks.style.width = '0%';
-        navLinks.style.visibility = 'hidden';
-    }
     
      
 })
