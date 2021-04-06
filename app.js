ee63d875dd8dc3eb5649b877a9a837b310dc3fba
Amine Amhoume <amhoume@gmail.com>
2021-04-06 22:27:38 +0100

    league teams needs responsive movbile, responsivness is almost done

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 
+// Side bar navigation menu code.
 const barMenu = document.getElementById('bar-menu')
 const navLinks = document.querySelector('.nav-links')
 // const movileView = window.matchMedia("(max-width:640px)")
@@ -9,12 +10,36 @@ barMenu.addEventListener('click', () => {
         navLinks.style.width = '40%';
         navLinks.style.visibility = "visible";
     
-     
 })
-
-
 
 closeMenu.addEventListener('click', () => {
     navLinks.style.width = '0%';
     navLinks.style.visibility = 'hidden'
 })
+
+//League fixtures of all matches code. 
+
+const leagueTitle = document.getElementById('league-name')
+const leagueMatche = Array.from(document.querySelectorAll('.league-mt'))
+const league = document.querySelector('.league')
+
+leagueTitle.addEventListener('click', () => {
+    leagueMatche.forEach(matche => matche.style.display = 'auto');
+}) 
+function addDisplay() {
+    leagueMatche.forEach(matche => {
+        if( matche.style.opacity === "0"){
+            matche.style.opacity = "1"
+            league.classList.add('show')
+        }else if( matche.style.opacity === "1"){
+            matche.style.opacity = "0"
+            league.classList.remove('show')
+        }else{
+            matche.style.opacity = "1"
+        }
+
+
+    });
+}
+
+leagueTitle.addEventListener('click', addDisplay)
