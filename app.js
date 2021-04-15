d834c7afa614178447d1cb0a74a6fd657aa6fecc
Amine Amhoume <amhoume@gmail.com>
2021-04-15 15:25:37 +0000

    Added some javascript fucntions

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 // Side bar navigation menu code.
 const barMenu = document.getElementById('bar-menu')
 const navLinks = document.querySelector('.nav-links')
-// const movileView = window.matchMedia("(max-width:640px)")
 const closeMenu = document.getElementById('close-menu')
 
 barMenu.addEventListener('click', () => {
@@ -17,29 +16,69 @@ closeMenu.addEventListener('click', () => {
     navLinks.style.visibility = 'hidden'
 })
 
-//League fixtures of all matches code. 
-
-const leagueTitle = document.getElementById('league-name')
-const leagueMatche = Array.from(document.querySelectorAll('.league-mt'))
-const league = document.querySelector('.league')
-
-leagueTitle.addEventListener('click', () => {
-    leagueMatche.forEach(matche => matche.style.display = 'auto');
-}) 
-function addDisplay() {
-    leagueMatche.forEach(matche => {
-        if( matche.style.opacity === "0"){
-            matche.style.opacity = "1"
-            league.classList.add('show')
-        }else if( matche.style.opacity === "1"){
-            matche.style.opacity = "0"
-            league.classList.remove('show')
-        }else{
-            matche.style.opacity = "1"
-        }
 
 
-    });
+//Get Matches of the day 
+
+const key = "9eee3620-99fa-11eb-9962-898dedcc3ac9" //free API key.
+const date_from = "2021-04-9"
+const date_to = "2021-04-12"
+
+//DOM elements.
+
+var conatiner = document.getElementById('container')
+
+
+
+async function getMatches() {
+
+        const req = await fetch(`https://app.sportdataapi.com/api/v1/soccer/matches?apikey=${key}&season_id=352&&date_from=${date_from}&date_to=${date_to}`)
+        const respn = await req.json();
+        return respn.data // return an array of objects
 }
 
-leagueTitle.addEventListener('click', addDisplay)
+
+getMatches().then(data => data.forEach(match => {
+        console.log(match)
+        //Home team data
+        const homeTeam = match.home_team;
+        const hometeamId = homeTeam.id;
+        const hometeamLogo = homeTeam.logo;
+        const hometeamScore = match.stats.home_score;
+        let awayteamName;
+        let hometeamName;
+
+        //Away team data
+        const awayTeam = match.away_team;
+        const awayteamLogo = awayTeam.logo;
+        const awayteamScore = match.stats.away_score;
+        
+        const awayteamId = awayTeam.id;
+
+        if(window.matchMedia("(max-width: 640px)").matches){
+            awayteamName = awayTeam.short_code;
+            hometeamName = homeTeam.short_code;
+        }else{
+            awayteamName = awayTeam.name;
+            hometeamName = homeTeam.name;
+        }
+        //matche info
+        const matchState = match.status;
+        const matchTime = match.match_start;
+
+        var game = document.createElement('div');
+        game.classList.add('matche')
+        game.innerHTML = `
+                    <div id="team1">
+                        <img id="team1" src="${hometeamLogo}" alt="">
+                        <span><a href="team.html">${hometeamName}</a></span>
+                    </div>
+                    <span id="time">21:00</span>     
+                    <div id="team2">
+                        <span><a href="team.html">${awayteamName}</a></span>
+                        <img src="${awayteamLogo}" alt="" id="team2">
+                    </div>
+        `
+        conatiner.appendChild(game)
+        
+}))
